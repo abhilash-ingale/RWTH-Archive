c4eac9a27613a893fcf3fe91a00a0f40f5f30e76
Abhilash Ingale <abhilashingale08@gmail.com>
2021-06-28 17:53:27 +0200

    Implemented data read func.

--- IMES/data_viz.m
+++ IMES/data_viz.m
@@ -1,25 +1,35 @@
-%%
+%% Clear workspace, command window
 clear;
 close all;
 clc;
 
 %%
+classf_feats = {'mcu_timestamp','bmi_accel_x', 'bmi_accel_y','bmi_accel_z','bmi_gyro_x','bmi_gyro_y','bmi_gyro_z'};
+
+empty_ds = read_and_clean("empty_imu.csv",classf_feats);
+wood_ds = read_and_clean("wood_imu.csv",classf_feats);
+plastic_ds = read_and_clean("plastic_imu.csv",classf_feats);
 
 
+%% APPENDIX: User-defined functions
 
-%%
-function importdata
-empty_imu_ds = datastore('empty_imu.csv','TreatAsMissing','NA','MissingValue',0);
+% 1. read_and_clean : 
 
-classification_features = {'mcu_timestamp', 'bmi_gyro_y'};
-empty_imu_ds.SelectedVariableNames = classification_features;
-empty_ds_clean = readall(empty_imu_ds)
+function temp_ds_array = read_and_clean(file_name,classf_feats)
+    
+    % Import .csv files into datastore
+    temp_ds = datastore(file_name,'TreatAsMissing','NA', 'MissingValue',0);
+    
+    % Convert selected features from datastore to array 
+    temp_ds.SelectedVariableNames = classf_feats;
+    temp_ds_array = table2array(readall(temp_ds));
+    
+    % Normalize data to start from 0
+    temp_ds_array(:,1) = temp_ds_array(:,1)- temp_ds_array(1,1);
+    
+    % Ignore rows with timestamp = 0 
+    posn = find(temp_ds_array(:,1)==0);
+    temp_ds_array(posn,:)= [];
+    
+end
 
-%%
-function plot_features
-plot(empty_ds_clean.mcu_timestamp, empty_ds_clean.bmi_gyro_y )
-
-%% 
-
-
-%% 
